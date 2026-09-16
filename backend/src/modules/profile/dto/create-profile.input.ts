0fce97e0f8a2fd2dfa708b342ddf55dc68a342e2
foxkodland <foxkodland@gmail.com>
2026-09-16 10:53:12 +0300

    add lib class-validator

--- backend/src/modules/profile/dto/create-profile.input.ts
+++ backend/src/modules/profile/dto/create-profile.input.ts
@@ -1,10 +1,18 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Field } from '@nestjs/graphql';
+import { IsString, IsNotEmpty, MinLength, MaxLength } from 'class-validator';
 
 @InputType()
 export class CreateProfileInput {
   @Field(() => String, { description: 'name' })
+  @IsString({ message: 'Имя должно быть строкой' })
+  @IsNotEmpty({ message: 'Имя не может быть пустым' })
+  @MinLength(2, { message: 'Имя слишком короткое' })
+  @MaxLength(100, { message: 'Имя слишком длинное' })
   name: string;
 
   @Field(() => String, { description: 'description' })
+  @IsString({ message: 'Имя должно быть строкой' })
+  @IsNotEmpty({ message: 'Имя не может быть пустым' })
+  @MaxLength(3000, { message: 'Описание слишком длинное' })
   description: string;
 }
