5199c7ea4ff5b2ceb55e09f5a95dbb630bdffc11
foxkodland <foxkodland@gmail.com>
2026-09-17 01:22:16 +0300

    The profile is available in the experience request

--- backend/src/modules/experience/experience.resolver.ts
+++ backend/src/modules/experience/experience.resolver.ts
@@ -1,8 +1,11 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, ResolveField, Parent } from '@nestjs/graphql';
 import { ExperienceService } from './experience.service.js';
 import { Experience } from './entities/experience.entity.js';
 import { CreateExperienceInput } from './dto/create-experience.input.js';
 import { UpdateExperienceInput } from './dto/update-experience.input.js';
+import { Profile } from '../profile/entities/profile.entity';
+import { profileRepository } from '@/database/repository/profile.repository';
+
 
 @Resolver(() => Experience)
 export class ExperienceResolver {
@@ -31,5 +34,10 @@ export class ExperienceResolver {
   @Mutation(() => Experience)
   async removeExperience(@Args('id', { type: () => Int }) id: number) {
     return await this.experienceService.remove(id);
+  }
+
+  @ResolveField(() => Profile, { name: 'profile' })
+  async getSkills(@Parent() experience: Experience) {
+    return await profileRepository.findById(experience.profile_id) 
   }
 }
